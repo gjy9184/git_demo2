bc489bf5be0dad4c06841cac02705a20cb26ffd1
gjy <gjy9184@163.com>
2023-10-24 12:13:03 +0800

    first commit

---
+++ src/main/java/org/example/git_use.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class git_use {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/git_use2.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class git_use2 {
+    public static void main(String[] args) {
+        System.out.println("git commit");
+    }
+}
+
