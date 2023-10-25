97dbf3f46e31bad83666b6c2c8af6738e5ec7f47
gjy <gjy9184@163.com>
2023-10-25 10:09:27 +0800

    hot-fix commit1

--- src/main/java/org/example/git_use2.java
+++ src/main/java/org/example/git_use2.java
@@ -4,6 +4,7 @@ public class git_use2 {
     public static void main(String[] args) {
         System.out.println("git commit");
         System.out.println("git commit2");
+        System.out.println("hot-fix");
     }
 }
 
