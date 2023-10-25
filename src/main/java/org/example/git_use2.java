f0409a198d5bc5a1f40137cd0418a797f7b9ca5b
gjy <gjy9184@163.com>
2023-10-25 09:50:49 +0800

    second commit

--- src/main/java/org/example/git_use2.java
+++ src/main/java/org/example/git_use2.java
@@ -3,6 +3,7 @@ package org.example;
 public class git_use2 {
     public static void main(String[] args) {
         System.out.println("git commit");
+        System.out.println("git commit2");
     }
 }
 
